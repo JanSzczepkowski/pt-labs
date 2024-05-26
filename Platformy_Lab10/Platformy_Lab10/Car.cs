33953023e58e8aa01ea3a31f6bccf86e833c3ff2
Jan <janszczepkowsky@gmail.com>
2024-05-26 23:34:53 +0200

    lab10 solution

---
+++ Platformy_Lab10/Platformy_Lab10/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Platformy_Lab10;
+
+namespace Platformy_Lab10;
+
+class Car
+{
+    public string Model { get; set; }
+    public Engine Motor { get; set; }
+    public int Year { get; set; }
+
+
+    public Car(string model, Engine motor, int year)
+    {
+        Model = model;
+        Motor = motor;
+        Year = year;
+    }
+
+    public Car() { }
+
+    override public string ToString()
+    {
+        return $"Car - Model: {Model}, Motor: {Motor}, Year: {Year}";
+    }
+}
---
+++ Platformy_Lab10/Platformy_Lab10/Engine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformy_Lab10
+{
+    class Engine : IComparable
+    {
+        public double Displacement { get; set; }
+        public double Horsepower { get; set; }
+        public string Model { get; set; }
+
+        public Engine() { }
+
+        public Engine(double displacement, double horsepower, string model)
+        {
+            Displacement = displacement;
+            Horsepower = horsepower;
+            Model = model;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj == null) return 1;
+            if (obj.GetType() != typeof(Engine)) return 1;
+            return Horsepower.CompareTo(((Engine)obj).Horsepower);
+        }
+    }
+}
